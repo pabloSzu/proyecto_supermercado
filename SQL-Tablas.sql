9dc7bff9772952a10aea02754f68921f319e4464
Pablo Szulman <pablo_szu@hotmail.com>
2024-03-19 18:27:52 -0300

    Corrección a Query de Creacion

--- SQL-Tablas.sql
+++ SQL-Tablas.sql
@@ -1,54 +1,56 @@
+
+
+-- Tabla de tipos de rankings
+CREATE TABLE Ranking (
+    id_ranking INT PRIMARY KEY IDENTITY,
+    nombre VARCHAR(50) NOT NULL,
+    valor_min INT,
+    valor_max INT
+);
+
 -- Tabla de proveedores
 CREATE TABLE Proveedores (
-    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    id_proveedor INT PRIMARY KEY IDENTITY,
     nombre VARCHAR(255),
     id_ranking INT,
-    FOREIGN KEY (id_ranking) REFERENCES Ranking(id_ranking),
+    FOREIGN KEY (id_ranking) REFERENCES Ranking(id_ranking)
 );
 
 -- Tabla de productos
 CREATE TABLE Productos (
-    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT PRIMARY KEY IDENTITY,
     codigo_barra VARCHAR(255),
-    nombre VARCHAR(255),
+    nombre VARCHAR(255)
 );
 
 -- Tabla de precios por proveedor y producto
 CREATE TABLE Precios (
-    id_precio INT AUTO_INCREMENT PRIMARY KEY,
+    id_precio INT PRIMARY KEY IDENTITY,
     id_proveedor INT,
     id_producto INT,
     precio DECIMAL(10,2),
-    FOREIGN KEY (idProveedor) REFERENCES Proveedores(idProveedor),
-    FOREIGN KEY (idProducto) REFERENCES Productos(idProducto),
+    FOREIGN KEY (id_proveedor) REFERENCES Proveedores(id_proveedor),
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto)
 );
 
 -- Tabla de pedidos
 CREATE TABLE Pedidos (
-    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    id_pedido INT PRIMARY KEY IDENTITY,
     id_proveedor INT,
     fechaPedido DATE,
-    estado ENUM('Pendiente', 'En proceso', 'Enviado', 'Entregado'),
+    estado VARCHAR(20), -- Cambiado a VARCHAR ya que SQL Server no soporta ENUM directamente
     fecha_entrega_prevista DATE,
     fechaEntrega_real DATE,
     evaluacion_proveedor INT,
-    FOREIGN KEY (idProveedor) REFERENCES Proveedores(idProveedor),
+    FOREIGN KEY (id_proveedor) REFERENCES Proveedores(id_proveedor)
 );
 
 -- Tabla de detalle de pedido
 CREATE TABLE DetallePedido (
-    id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+    id_detalle INT PRIMARY KEY IDENTITY,
     id_pedido INT,
     id_producto INT,
     cantidad INT,
-    FOREIGN KEY (idPedido) REFERENCES Pedidos(idPedido),
-    FOREIGN KEY (idProducto) REFERENCES Productos(idProducto),
-);
-
--- Tabla de tipos de rankings
-CREATE TABLE Ranking (
-    id_ranking INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    valor_min INT,
-    valor_max INT,
+    FOREIGN KEY (id_pedido) REFERENCES Pedidos(id_pedido),
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto)
 );
